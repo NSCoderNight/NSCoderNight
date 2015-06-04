247104bc35189417f8a6a51bff57f3848c54d385
Samuel E. Giddins <segiddins@segiddins.me>
2015-06-03 22:00:40 -0700

    Make organizers and meetings arrays

--- lib/chapter.rb
+++ lib/chapter.rb
@@ -1,25 +1,61 @@
 require 'active_support/core_ext/hash'
 require 'active_support/core_ext/string'
+require 'active_support/inflector'
 require 'pathname'
 require 'yaml'
 
+module FromHash
+  ATTRS = [].freeze
+
+  def initialize(hash)
+    self.class::ATTRS.each do |a|
+      value = hash[a]
+      if cls = self.class.const_defined?(a.to_s.singularize.classify) && self.class.const_get(a.to_s.singularize.classify)
+        value = Array(value).map { |v| cls.new(v) } if value.nil? || value.is_a?(Array)
+      end
+      instance_variable_set(:"@#{a}", value)
+    end
+  end
+
+  def FromHash.included(mod)
+    mod::ATTRS.each { |a| mod.send(:attr_reader, a) }
+  end
+end
+
 class Chapter
+  class Organizer
+    ATTRS = %i(
+      name
+      email
+      twitter
+      website
+    ).freeze
+
+    include FromHash
+  end
+
+  class Meeting
+    ATTRS = %i(
+      name
+      day
+      time
+      location
+    ).freeze
+
+    include FromHash
+  end
+
   ATTRS = %i(
-    location
     name
-    meeting_time
     twitter
     province
     country
     website
-    day
-    organizer_name
-    organizer_email
-  	organizer_twitter
-  	organizer_website
+    organizers
+    meetings
   ).freeze
 
-  ATTRS.each { |a| attr_reader a }
+  include FromHash
 
   def self.from_file(file)
     begin
@@ -28,12 +64,7 @@ class Chapter
       raise "Unable to parse the chapter info from '#{file.relative_path_from(Pathname.pwd)}'"
     end
 
-    new.tap do |chapter|
-      ATTRS.each do |a|
-        value = yaml[a]
-        chapter.instance_variable_set(:"@#{a}", value)
-      end
-    end
+    new(yaml)
   end
 
   def self.all
